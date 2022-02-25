aa14ab08c102d174fedb0c466a22bb0842aed502
bxxmi <bmi6638@gmail.com>
2022-02-25 17:59:01 +0900

    style: add theme styles

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,6 +3,20 @@ const viewSize = {
   height: '552px',
 }
 
+const fontSize = {
+  large: '36px',
+  medium: '15px',
+  small: '14px',
+}
+
+const colors = {
+  black: '#3a3a3a',
+  darkGray: '#3a3a3acc',
+  lightGray: '#3a3a3ab3',
+}
+
 export const theme = {
   viewSize,
+  fontSize,
+  colors,
 }
