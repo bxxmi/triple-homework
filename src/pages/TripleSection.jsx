f1e498a49b705644d6890b07e7491678ab6a2bf0
bxxmi <bmi6638@gmail.com>
2022-02-25 23:18:42 +0900

    style: add section style

--- src/pages/TripleSection.jsx
+++ src/pages/TripleSection.jsx
@@ -1,5 +1,19 @@
+import TrophyContent from '../views/TrophyContent'
+import AwardInfoContent from '../views/AwardInfoContent'
+import styled from 'styled-components'
+
 const TripleSection = () => {
-  return <div />
+  return (
+    <SectionContainer>
+      <TrophyContent />
+      <AwardInfoContent />
+    </SectionContainer>
+  )
 }
 
+const SectionContainer = styled.div`
+  padding-top: 150px;
+  display: flex;
+  justify-content: space-evenly;
+`
 export default TripleSection
