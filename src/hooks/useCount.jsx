cb15e006c0ff0a5bbb95621a1da6947da44308cc
bxxmi <bmi6638@gmail.com>
2022-02-27 18:12:14 +0900

    feat: implement the same time end

--- src/hooks/useCount.jsx
+++ src/hooks/useCount.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 
 const useCount = (startNumber, endNumber) => {
   const [currentNumber, setCurrentNumber] = useState(startNumber)
-  const runTime = 2000 / (endNumber - startNumber)
+  const runTime = Math.round(2000 / endNumber)
 
   const handleIncrease = () => {
     const newCurrentNumber =
