7710c1a4037e11f76d0ed3e0b920e75fd107ac20
bxxmi <bmi6638@gmail.com>
2022-02-28 17:01:09 +0900

    refactor: remove css effect for use common components

--- src/pages/TripleSection.jsx
+++ src/pages/TripleSection.jsx
@@ -1,13 +1,16 @@
 import TrophyContent from '../views/TrophyContent'
 import AwardInfoContent from '../views/AwardInfoContent'
 import styled from 'styled-components'
+import FadeInContainer from '../components/FadeInContainer'
 
 const TripleSection = () => {
   return (
-    <SectionContainer>
-      <TrophyContent />
-      <AwardInfoContent />
-    </SectionContainer>
+    <FadeInContainer>
+      <SectionContainer>
+        <TrophyContent />
+        <AwardInfoContent />
+      </SectionContainer>
+    </FadeInContainer>
   )
 }
 
@@ -15,19 +18,6 @@ const SectionContainer = styled.div`
   padding-top: 150px;
   display: flex;
   justify-content: space-evenly;
-  animation: fadeIn 700ms ease-in;
-
-  @keyframes fadeIn {
-    from {
-      opacity: 0;
-      transform: translateY(15px);
-    }
-
-    to {
-      opacity: 1;
-      transform: translateY(0);
-    }
-  }
 `
 
 export default TripleSection
