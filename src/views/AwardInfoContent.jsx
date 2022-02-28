de3cc53790c1670f434bae2b914d5dd969dd91d6
bxxmi <bmi6638@gmail.com>
2022-02-28 19:21:20 +0900

    style: remove css effect for use common components

--- src/views/AwardInfoContent.jsx
+++ src/views/AwardInfoContent.jsx
@@ -1,31 +1,13 @@
-import styled from 'styled-components'
 import StoreAward from './StoreAward'
 import UsageCount from './UsageCount'
 
 const AwardInfoContent = () => {
   return (
-    <InfoContainer>
+    <div>
       <UsageCount />
       <StoreAward />
-    </InfoContainer>
+    </div>
   )
 }
-
-const InfoContainer = styled.div`
-  animation: fadeIn 700ms ease-in;
-  animation-delay: 100ms;
-
-  @keyframes fadeIn {
-    from {
-      opacity: 0;
-      transform: translateY(15px);
-    }
-
-    to {
-      opacity: 1;
-      transform: translateY(0);
-    }
-  }
-`
 
 export default AwardInfoContent
